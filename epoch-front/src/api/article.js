ad94aa9b398632fe3be8d9d6aadfcccf524f149f
Marshal <453607106@qq.com>
2019-10-30 21:44:18 +0800

    [add]-前端测试

--- epoch-front/src/api/article.js
+++ epoch-front/src/api/article.js
@@ -1,41 +1,33 @@
 import request from '@/utils/request'
 
-export function fetchList(pagination,queryParam) {
+export function query(page, pageSize, queryParam) {
   return request({
-    url: '/base/role/query/' + pagination.page + '/' + pagination.pageSize + '',
+    url: '/base/role/query/' + page + '/' + pageSize + '',
     method: 'post',
     data: queryParam
   })
 }
 
-export function fetchArticle(id) {
+export function submit(dto) {
   return request({
-    url: '/article/detail',
+    url: '/base/role/submit',
+    method: 'post',
+    data: dto
+  })
+}
+
+export function remove(idList) {
+  return request({
+    url: '/base/role/remove',
+    method: 'post',
+    data: idList
+  })
+}
+
+export function queryById(id) {
+  return request({
+    url: '/base/role/queryById',
     method: 'get',
     params: {id}
-  })
-}
-
-export function fetchPv(pv) {
-  return request({
-    url: '/article/pv',
-    method: 'get',
-    params: {pv}
-  })
-}
-
-export function createArticle(data) {
-  return request({
-    url: '/article/create',
-    method: 'post',
-    data
-  })
-}
-
-export function updateArticle(data) {
-  return request({
-    url: '/article/update',
-    method: 'post',
-    data
   })
 }
