528a16f0452c92ace77a09f4efcbf22fb55aeec7
Marshal <453607106@qq.com>
2019-10-31 23:19:35 +0800

    [fixed]-解决element-ui的table表格控件表头与内容列不对齐问题

--- epoch-front/src/App.vue
+++ epoch-front/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
+
+<style>
+  /* 解决element-ui的table表格控件表头与内容列不对齐问题 add by Marshal 2019/10/31 */
+  .el-table th.gutter {
+    display: table-cell !important;
+  }
+</style>
