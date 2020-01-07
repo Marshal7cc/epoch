bba88cac016b9c057a6558b8771dd7d6f04949c9
Marshal <453607106@qq.com>
2020-01-08 00:26:35 +0800

    [update]-commit

--- epoch-test/src/main/java/com/marshal/epoch/test/TestApplication.java
+++ epoch-test/src/main/java/com/marshal/epoch/test/TestApplication.java
@@ -1,5 +1,6 @@
 package com.marshal.epoch.test;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/12/16
  * @desc
  */
+@EnableSwagger2Doc
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args) {
