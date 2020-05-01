aa0db360bd8231fdfab4f9ce61d98d252b881150
Marshal <453607106@qq.com>
2020-05-01 21:35:06 +0800

    [add]-epoch security starter,oauth2认证中心基于RBAC鉴权机制添加

--- epoch-component/epoch-security-starter/src/main/java/com/marshal/epoch/security/config/ResourceServerConfig.java
+++ epoch-component/epoch-security-starter/src/main/java/com/marshal/epoch/security/config/ResourceServerConfig.java
@@ -1,24 +1,32 @@
-package com.marshal.epoch.system.config;
+package com.marshal.epoch.security.config;
 
-
-import com.marshal.epoch.auth.handler.EpochAccessDeniedHandler;
-import com.marshal.epoch.auth.handler.EpochAuthenticationEntryPoint;
+import com.marshal.epoch.security.handler.EpochAccessDeniedHandler;
+import com.marshal.epoch.security.handler.EpochAuthenticationEntryPoint;
+import com.marshal.epoch.security.properties.SecurityProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
-
 /**
  * @auth: Marshal
  * @date: 2020/1/15
- * @desc:
+ * @desc: 资源服务器认证配置
  */
 @Configuration
 @EnableResourceServer
+@EnableConfigurationProperties(SecurityProperty.class)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private SecurityProperty securityProperty;
+
+    @Autowired
+    private AccessDecisionManager accessDecisionManager;
 
     @Autowired
     private EpochAccessDeniedHandler accessDeniedHandler;
@@ -28,14 +36,22 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
         http.csrf().disable()
-                .requestMatchers().antMatchers("/**")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/oauth").permitAll()
-                .antMatchers("/prompt/i18n").permitAll()
+                .requestMatchers()
+                .antMatchers("/**");
+
+        //白名单
+        String[] whiteList = securityProperty.getWhiteList();
+        if (whiteList != null) {
+            http.authorizeRequests().antMatchers(whiteList).permitAll();
+        }
+
+        http.authorizeRequests()
                 .antMatchers("/**").authenticated()
+                .accessDecisionManager(accessDecisionManager)
                 .and().httpBasic();
+
     }
 
     @Override
@@ -43,6 +59,5 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         resources.authenticationEntryPoint(exceptionEntryPoint)
                 .accessDeniedHandler(accessDeniedHandler);
     }
-
 }
 
