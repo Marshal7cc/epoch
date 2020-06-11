db16b53fd89a2eb89e60384767ab43fef9549c38
Marshal <453607106@qq.com>
2020-06-11 20:26:16 +0800

    [fix]-暂时解决异常时导致转发error的401问题

--- epoch-instrument/epoch-starter-security/src/main/java/com/marshal/epoch/security/SecurityAutoConfiguration.java
+++ epoch-instrument/epoch-starter-security/src/main/java/com/marshal/epoch/security/SecurityAutoConfiguration.java
@@ -9,11 +9,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.provider.expression.OAuth2WebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.filter.RequestContextFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +43,18 @@ public class SecurityAutoConfiguration {
     @ConditionalOnMissingBean
     public EpochAccessDeniedHandler accessDeniedHandler() {
         return new EpochAccessDeniedHandler();
+    }
+
+    /**
+     * todo: fix me by update version or other way
+     * 暂时解决异常时自动转发至/error导致的401问题
+     *
+     * @return OAuth2ClientContext
+     */
+    @Bean
+    @Primary
+    public OAuth2ClientContext oAuth2ClientContext() {
+        return new DefaultOAuth2ClientContext();
     }
 
     @Bean
