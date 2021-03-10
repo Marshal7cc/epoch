06df5bb6af56fcee66fee814d99515ed853fbf4c
Marshal <453607106@qq.com>
2021-03-10 22:53:52 +0800

    feat:add mapper auto scan

--- epoch-starters/epoch-starter-mybatis/src/main/java/org/epoch/mybatis/MybatisAutoConfiguration.java
+++ epoch-starters/epoch-starter-mybatis/src/main/java/org/epoch/mybatis/MybatisAutoConfiguration.java
@@ -15,11 +15,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author Marshal
  * @date 2020/4/20
  */
+@MapperScan("org.epoch.**.mapper")
 @ComponentScan(basePackages = "org.epoch.mybatis.**")
 @EnableConfigurationProperties({MybatisProperties.class})
 public class MybatisAutoConfiguration {
