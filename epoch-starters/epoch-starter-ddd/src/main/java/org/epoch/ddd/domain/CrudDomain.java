ef9910dfcccca74a07d1b10f5df20ce8177f7114
Marshal <453607106@qq.com>
2022-07-14 00:04:20 +0800

    feat: add crudDomain interface

---
+++ epoch-starters/epoch-starter-ddd/src/main/java/org/epoch/ddd/domain/CrudDomain.java
@@ -0,0 +1,11 @@
+package org.epoch.ddd.domain;
+
+import org.epoch.data.domain.Auditable;
+import org.epoch.data.repository.BaseRepository;
+
+/**
+ * @author Marshal
+ * @since 2022/7/14
+ */
+public interface CrudDomain<R extends BaseRepository<T, ID>, T extends Auditable<ID>, ID> {
+}
