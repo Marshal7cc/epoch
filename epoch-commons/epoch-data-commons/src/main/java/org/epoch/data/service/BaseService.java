cd712afeac9e68aa5aa97e51da0505f2959c9ccb
Marshal <453607106@qq.com>
2022-07-22 22:23:53 +0800

    feat: add BaseService in data commons

--- epoch-commons/epoch-data-commons/src/main/java/org/epoch/data/service/BaseService.java
+++ epoch-commons/epoch-data-commons/src/main/java/org/epoch/data/service/BaseService.java
@@ -1,27 +1,115 @@
 package org.epoch.data.service;
 
+import java.util.List;
+
 import org.epoch.data.domain.Page;
-import org.epoch.data.repository.BaseRepository;
 import org.springframework.data.domain.Pageable;
 
 /**
  * @author Marshal
  * @since 2022/7/20
  */
-public interface BaseService<D, T,ID> {
+public interface BaseService<DOMAIN, ID> {
+
+    /**
+     * Saves a given domain object by repository.
+     *
+     * @param domain
+     * @return
+     */
+    DOMAIN save(DOMAIN domain);
 
 
-    @SuppressWarnings("rawtypes")
-    BaseRepository<T,ID> getRepository();
-
-    D save(D domain);
-
-
-    D findById(ID id);
-
-
-    void removeById(ID id);
+    /**
+     * Saves all given domain objects by repository.
+     *
+     * @param domains
+     * @return
+     */
+    List<DOMAIN> saveAll(List<DOMAIN> domains);
 
 
-    <Q> Page<D> findAll(Pageable pageable, Q query);
+    /**
+     * Retrieves an domain object by its id.
+     *
+     * @param id
+     * @return
+     */
+    DOMAIN findById(ID id);
+
+    /**
+     * Returns whether an domain object with the given id exists.
+     *
+     * @param id
+     * @return
+     */
+    boolean existsById(ID id);
+
+
+    /**
+     * Returns all domain objects of the type.
+     *
+     * @return
+     */
+    List<DOMAIN> findAll();
+
+    /**
+     * Returns all domain objects with the given IDs.
+     *
+     * @param ids
+     * @return
+     */
+    List<DOMAIN> findAllById(List<ID> ids);
+
+
+    /**
+     * Returns all domain objects meeting the restriction provided in the {@code Query} object.
+     *
+     * @param query
+     * @param <Q>
+     * @return
+     */
+    <Q> List<DOMAIN> findAll(Q query);
+
+    /**
+     * Returns the number of domain objects available.
+     *
+     * @return the number of domain objects.
+     */
+    long count();
+
+    /**
+     * Deletes the domain object with the given id.
+     *
+     * @param id must not be {@literal null}.
+     */
+    void deleteById(ID id);
+
+    /**
+     * Deletes a given domain object.
+     *
+     * @param domain must not be {@literal null}.
+     */
+    void delete(DOMAIN domain);
+
+    /**
+     * Deletes the given domain objects.
+     *
+     * @param domains must not be {@literal null}. Must not contain {@literal null} elements.
+     */
+    void deleteAll(List<DOMAIN> domains);
+
+    /**
+     * Deletes all domain objects managed by the repository.
+     */
+    void deleteAll();
+
+    /**
+     * Returns a {@link Page} of domain objects meeting the paging restriction provided in the {@code Pageable} object and the {@code Query} object.
+     *
+     * @param pageable
+     * @param query
+     * @return a page of domain objects
+     */
+    <Q> Page<DOMAIN> findAll(Pageable pageable, Q query);
 }
--- epoch-commons/epoch-data-commons/src/main/java/org/epoch/data/service/impl/BaseServiceImpl.java
+++ epoch-commons/epoch-data-commons/src/main/java/org/epoch/data/service/impl/BaseServiceImpl.java
@@ -1,7 +1,8 @@
 package org.epoch.data.service.impl;
 
+import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 import org.epoch.core.util.BaseConverter;
 import org.epoch.data.domain.Page;
@@ -15,45 +16,86 @@ import org.springframework.data.domain.Pageable;
  * @author Marshal
  * @since 2022/7/21
  */
-public class BaseServiceImpl<R extends BaseRepository<T, ID>, D, T, ID> implements BaseService<D, T, ID> {
-    protected final Class<D> dClass;
-    protected final Class<T> tClass;
+public class BaseServiceImpl<R extends BaseRepository<T, ID>, DO, T, ID extends Serializable> implements BaseService<DO, ID> {
 
     @Autowired
     protected R repository;
+    protected final Class<T> entityClass;
+    protected final Class<DO> domainClass;
 
     @SuppressWarnings("unchecked")
     protected BaseServiceImpl() {
-        this.dClass = (Class<D>) Objects.requireNonNull(GenericTypeResolver.resolveTypeArguments(this.getClass(), BaseServiceImpl.class))[1];
-        this.tClass = (Class<T>) Objects.requireNonNull(GenericTypeResolver.resolveTypeArguments(this.getClass(), BaseServiceImpl.class))[2];
+        this.domainClass = (Class<DO>) Objects.requireNonNull(GenericTypeResolver.resolveTypeArguments(this.getClass(), BaseServiceImpl.class))[1];
+        this.entityClass = (Class<T>) Objects.requireNonNull(GenericTypeResolver.resolveTypeArguments(this.getClass(), BaseServiceImpl.class))[2];
     }
 
     @Override
-    public BaseRepository<T, ID> getRepository() {
-        return repository;
-    }
-
-    @Override
-    public D save(D domain) {
-        T entity = BaseConverter.parseObject(domain, tClass);
+    public DO save(DO domain) {
+        T entity = BaseConverter.parseObject(domain, entityClass);
         repository.saveOne(entity);
-        return domain;
+        return BaseConverter.parseObject(entity, domainClass);
     }
 
     @Override
-    public D findById(ID id) {
-        Optional<T> optional = repository.findById(id);
-        return optional.map(t -> BaseConverter.parseObject(t, dClass)).orElse(null);
+    public List<DO> saveAll(List<DO> domains) {
+        List<T> entities = BaseConverter.parseArray(domains, entityClass);
+        repository.saveAll(entities);
+        return BaseConverter.parseArray(entities, domainClass);
     }
 
     @Override
-    public void removeById(ID id) {
+    public DO findById(ID id) {
+        return repository.findById(id).map(t -> BaseConverter.parseObject(t, domainClass)).orElse(null);
+    }
+
+    @Override
+    public boolean existsById(ID id) {
+        return repository.existsById(id);
+    }
+
+    @Override
+    public List<DO> findAll() {
+        return BaseConverter.parseArray(repository.findAll(), domainClass);
+    }
+
+    @Override
+    public List<DO> findAllById(List<ID> ids) {
+        return BaseConverter.parseArray(repository.findAllById(ids), domainClass);
+    }
+
+    @Override
+    public <Q> List<DO> findAll(Q query) {
+        return BaseConverter.parseArray(repository.findAll(query), domainClass);
+    }
+
+    @Override
+    public long count() {
+        return repository.count();
+    }
+
+    @Override
+    public void deleteById(ID id) {
         repository.deleteById(id);
     }
 
     @Override
-    public <Q> Page<D> findAll(Pageable pageable, Q query) {
+    public void delete(DO domain) {
+        repository.delete(BaseConverter.parseObject(domain, entityClass));
+    }
+
+    @Override
+    public void deleteAll(List<DO> domains) {
+        repository.deleteAll(BaseConverter.parseArray(domains, entityClass));
+    }
+
+    @Override
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+
+    @Override
+    public <Q> Page<DO> findAll(Pageable pageable, Q query) {
         Page<T> tPage = repository.findAll(pageable, query);
-        return new Page<>(tPage.getPageInfo(), BaseConverter.parseArray(tPage.getContent(), dClass));
+        return new Page<>(tPage.getPageInfo(), BaseConverter.parseArray(tPage.getContent(), domainClass));
     }
 }
