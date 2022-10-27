90b376ac42616b133483cd1f6c4c8fa6f7a2171b
Marshal <453607106@qq.com>
2022-10-27 22:20:35 +0800

    feat: [fix] remove useless annotation

--- epoch-commons/epoch-core-commons/src/main/java/org/epoch/core/autoconfigure/CoreAutoConfiguration.java
+++ epoch-commons/epoch-core-commons/src/main/java/org/epoch/core/autoconfigure/CoreAutoConfiguration.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.epoch.core.util.ApplicationContextHelper;
 import org.epoch.core.util.GenericTypeConverter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Marshal
  * @date 2020/5/30
  */
-@Configuration
 public class CoreAutoConfiguration {
     @Bean
     public ApplicationContextHelper applicationContextHolder() {
